67c255f8175a14360a51f4beb8bbf74703e00302
Omar El Yousfi <elyousfiomarr@gmail.com>
2026-03-22 22:41:01 -0300

    add initial generic & singular data tests

--- prescriptions_data_builder/models/dimensions/dim_medications.sql
+++ prescriptions_data_builder/models/dimensions/dim_medications.sql
@@ -1,20 +1,26 @@
-with deduped_medications as (
-    select distinct * 
-    from {{ref('stg_prescriptions_orders')}}
+{{ config(
+    materialized='incremental',
+    unique_key='medication_id'
+) }}
+
+with source_medications as (
+    select *
+    from {{ ref('stg_medications') }}
 ),
 dim_medications as (
-select 
-    medication_sk, 
-    medication_name,
-    form,
-    strength,
-    drug_identification_number,
-    current_timestamp as added_at
-from deduped_medications
-{% if is_incremental() %}
+    select
+        md5(medication_name || form || strength || drug_identification_number) as medication_sk,
+        medication_id,
+        medication_name,
+        form,
+        strength,
+        drug_identification_number,
+        current_timestamp as added_at
+    from source_medications
+    {% if is_incremental() %}
         -- Only insert medications that do NOT already exist
-        where din not in (
-            select din from {{ this }}
+        where medication_id not in (
+            select medication_id from {{ this }}
         )
     {% endif %}
 )
--- prescriptions_data_builder/models/staging/stg_medications.sql
+++ prescriptions_data_builder/models/staging/stg_medications.sql
@@ -1,8 +1,10 @@
-select
+-- Extract distinct medications from source, removing prescription context
+-- Medications should be dimension data independent of any single prescription
+select distinct
     medication_id,
-    prescription_id,
     medication_name,
     form,
     strength,
     lpad(din, 8, '0') as drug_identification_number
-from pharmacy_db.public.medications
+from {{ source('source_data', 'medications') }}
+order by medication_id
